9c7303803d5576193fad73bb08c4d1d532d73f1a
david rosa <rosa_d@qwasar.io>
2022-11-14 07:16:34 +0000

    c-arc01-mastermind: Add comments and improve input validation.

--- main.c
+++ main.c
@@ -9,48 +9,36 @@
 # define NUM_PIECES 9
 
 typedef struct s_game_info {
-    bool pieces[NUM_PIECES];
-    char* code;
     unsigned int attempts;
     unsigned int well_placed;
     unsigned int misplaced;
+    char* code;
     bool won;
+    bool* pieces;
 } game_info;
 
 /*
-*   Initializes values in a bit map to zero
-*   and sets secret code pieces to one.
+*   Generates a randomized secret code,
+*   using the "current time" as the seed
+*   value for the random number generator.
+*   It will always generate a 4-digit random
+*   integer and convert it to a string
+*   while replacing 9s with 0s.
+*   
 */
-
-void init_secret_pieces(char* code, bool* pieces) {
-    unsigned int i = 0;
-    while (*code) {
-        pieces[i] = false;
-        pieces[i + 1] = false;
-        pieces[*code - '0'] = true;
-        code++;
-        i += 2;
-    }
-    pieces[i] = 0;
-}
-
-/*
-*   Generates a randomized secret code.
-*/
-void generate_secret_code(char* secret_code) {
+char* generate_secret_code(char* secret_code) {
     unsigned int num;
-    // uses the "current time" as the seed
-    // value for the random number generator. 
+
     srand(time(0));
-    // generates a 4-digit random integer.
     do {
         num = rand() % 10000;
     } while (num < 1000);
-    // converts the given number to a string
-    for (int i = 0; i < CODE_SIZE; i++) {
-        secret_code[i] = '0' + num % 10;
+
+    for (int i = CODE_SIZE - 1; i >= 0 ; i--) {
+        secret_code[i] = '0' + (num % 10 % 9);
         num /= 10;
     }
+    return secret_code;
 }
 
 /*
@@ -58,32 +46,23 @@ void generate_secret_code(char* secret_code) {
 *   secret code or number of attempts if any.
 *   Otherwise, it generates a randomized secret
 *   code and sets number of attempts to MAX_ATTEMPTS.
+*   Lastly, it initializes a bit map, setting
+*   secret code pieces to one and the rest to zero
+*   to make it easier to find misplaced pieces.
 */
 void init_game(char** argv, game_info* game) {
-    if (argv[1] && argv[1][1] == 'c') {
-        game->code = argv[2];
-    }
-    else if (argv[3] && argv[3][1] == 'c') {
-        game->code = argv[4];
-    }
-    else {
-        generate_secret_code(game->code);
-    }
-
-    init_secret_pieces(game->code, game->pieces);
-
-    if (argv[3] && argv[3][1] == 't') {
-        game->attempts = atoi(argv[4]);
-    }
-    else if (argv[1] && argv[1][1] == 't') {
-        game->attempts = atoi(argv[2]);
-    }
-    else {
-        game->attempts = MAX_ATTEMPTS;
-    }
+    game->code =    argv[1] && argv[2] && argv[1][1] == 'c' ? argv[2] :
+                    argv[3] && argv[4] && argv[3][1] == 'c' ? argv[4] :
+                    generate_secret_code(game->code);
+    game->attempts =    argv[3] && argv[4] && argv[3][1] == 't' ? atoi(argv[4]) :
+                        argv[1] && argv[2] && argv[1][1] == 't' ? atoi(argv[2]) :
+                        MAX_ATTEMPTS;
     game->well_placed = 0;
     game->misplaced = 0;
     game->won = false;
+
+    for (int i = 0; i < CODE_SIZE; i++)
+        game->pieces[game->code[i] - '0'] = true;
 }
 
 void print_bit_map(bool bits[NUM_PIECES]) {
@@ -92,7 +71,6 @@ void print_bit_map(bool bits[NUM_PIECES]) {
 }
 
 bool check_guess(char* guess, game_info* game) {
-    // printf("guess: %s\n", guess);
     for (int i = 0; guess[i]; i ++) {
         if (guess[i] == game->code[i]) {
             game->well_placed++;
@@ -104,8 +82,13 @@ bool check_guess(char* guess, game_info* game) {
     return game->well_placed == CODE_SIZE;
 }
 
-bool validate_input(char* input) {
+bool validate_input(char* input, int n) {
     int i = 0;
+
+    // Return false if guess is not exactly CODE_SIZE characters.
+    if(!((n == CODE_SIZE && input[CODE_SIZE] == '\0') || n == CODE_SIZE + 1))
+        return false;
+
     while(input[i]) {
         if (input[i] < '0' || input[i] > '8')
             return false;
@@ -117,21 +100,20 @@ bool validate_input(char* input) {
 int main(int ac, char** argv) {
     game_info game;
     char guess[100];
-    char secret_code[5] = "0000";
+    char secret_code[] = "0000";
+    bool pieces[] = {0,0,0,0,0,0,0,0,0};
     game.code = secret_code;
+    game.pieces = pieces;
     unsigned int n = 1;
     unsigned int round = 0;
     bool valid_input = true;
 
-    //TODO fix bug where 
-    // If ./my_mastermind -c 4321
-    // guess 1234 shows 3 misplaced pieces
-    // instead of 4!
-    
     init_game(argv, &game);
-    // TODO validate game code
-    // && validate_input(game.code)
-    if (ac) {
+    print_bit_map(game.pieces);
+    printf("\n%s\n", game.code);
+    printf("%d\n", game.attempts);
+
+    if ((ac == 3 || ac == 5) && validate_input(game.code, CODE_SIZE)) {
 
         printf("Will you find the secret code?\n");
         printf("Please enter a valid guess\n");
@@ -144,13 +126,15 @@ int main(int ac, char** argv) {
 
             n = read(0, guess, 100);
 
-            if (n == 4 && guess[CODE_SIZE] == 0) {
+            // Print a newline if the user pressed Ctrl + D.
+            // or replace the newline character with '\0'
+            // if the user entered CODE_SIZE characters and pressed Enter.
+            if (guess[n - 1] != '\n')
                 printf("\n");
-                n++;
-            }
+            else if (n == CODE_SIZE + 1 && guess[CODE_SIZE] == '\n')
+                guess[CODE_SIZE] = '\0';
 
-            guess[CODE_SIZE] = '\0';
-            if (n == 5 && validate_input(guess)) {
+            if (validate_input(guess, n)) {
                 game.won = check_guess(guess, &game);
                 valid_input = true;
             }
@@ -159,6 +143,7 @@ int main(int ac, char** argv) {
                 valid_input = false;
                 continue;
             }
+
             if (game.won) {
                 printf("Congratz! You did it!\n");
                 break;
